267f121a73eb1a9a9bab6bb0de81208def7b9a39
Jargaltogtokh <Jagaa155@gmail.com>
2024-12-13 18:06:53 +0800

    updated

--- src/app/component/card.jsx
+++ src/app/component/card.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-export function Card({ value }) {
+export function Card({ cityName, value, temperature, condition }) {
   const nightColor = "bg-gradient-to-b from-[#1F2937] to-[#111827] text-white";
   const color = value === "night" ? nightColor : "bg-white";
   const cardBackground = value === "night" ? "bg-[#111827]" : "bg-white";
@@ -12,9 +12,15 @@ export function Card({ value }) {
       <div
         className={`w-[414px] h-[868px] bg-white rounded-[78px] mt-[10px] ${color}`}
       >
-        <div className={"mt-[56px] ml-[100px]"}>
+        <div className={"mt-[56px] ml-[50px]"}>
           <p className="text-[24px] text-[#9CA3AF]"> December 12, 2024 </p>
-          <h1 className="text-[50px] font-extrabold"> Tokyo</h1>
+          <h1 className="text-[50px] font-extrabold"> {cityName}</h1>
+          <h1 className="text-[100px] font-extrabold mt-[380px] flex ">
+            {temperature}
+          </h1>
+          <h2 className="text-[24px] text-[#FF8E27] font-bold flex ">
+            {condition}
+          </h2>
         </div>
       </div>
     </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,83 @@
-/** @type {import('tailwindcss').Config} */
-
+"use client";
+import { useState, useEffect } from "react";
 import { Card } from "./component/card";
 import { SearchIcon } from "./component/SearchIcon";
 
+const API_KEY = "ee316010829b4d2fbdc72448241312";
+
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState("Ulaanbaatar");
+  const [dayWeather, setDayWeather] = useState({
+    temperature: null,
+    condition: null,
+  });
+
+  const [nightWeather, setNightWeather] = useState({
+    temperature: null,
+    condition: null,
+  });
+
+  const [isClient, setIsClient] = useState(false);
+
+  const onChangeText = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const onPressEnter = (e) => {
+    if (e.code === "Enter") {
+      setCity(search);
+    }
+  };
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  useEffect(() => {
+    fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=1&aqi=no&alerts=no`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setDayWeather({
+          temperature: data.forecast.forecastday[0].day.maxtemp_c,
+          condition: data.forecast.forecastday[0].day.condition.text,
+        });
+        setNightWeather({
+          temperature: data.forecast.forecastday[0].day.mintemp_c,
+          condition: data.forecast.forecastday[0].day.condition.text,
+        });
+
+        console.log(data.current.temp_c, "---Temperature");
+        console.log(data.current.condition.text, "---Condition");
+      });
+  }, [city]);
+
+  if (!isClient) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="w-full h-screen flex rounded-3xl">
-      <div className="w-[50%] h-screen bg-stone-50 flex flex-col-reverse items-center justify-between pb-[82px] rounded-3xl">
-        <Card value="day" />
-        <SearchInput />
+      <div className="w-[60%] h-screen bg-stone-50 flex flex-col-reverse items-center justify-between pb-[82px] rounded-3xl">
+        <Card
+          value="day"
+          temperature={dayWeather.temperature}
+          condition={dayWeather.condition}
+          cityName={city}
+        />
+        <SearchInput
+          search={search}
+          onChangeText={onChangeText}
+          onPressEnter={onPressEnter}
+        />
       </div>
       <div className="w-[50%] h-screen bg-[#0F141E] flex flex-col-reverse items-center justify-between pb-[82px] rounded-3xl">
-        <Card value="night" />
+        <Card
+          value="night"
+          temperature={nightWeather.temperature}
+          condition={nightWeather.condition}
+          cityName={city}
+        />
       </div>
     </div>
   );
@@ -19,11 +85,11 @@ export default function Home() {
 
 const Cirle = () => {
   return (
-    <div className="absolute rounded-full w-36 h-36 bg-red-200 flex flex-col-reverse items-center justify-center"></div>
+    <div className="absolute rounded-full w-36 h-36 bg-red-200 flex"></div>
   );
 };
 
-const SearchInput = () => {
+const SearchInput = ({ search, onChangeText, onPressEnter }) => {
   return (
     <div className="h-12 w-[500px] bg-white rounded-3xl mt-5 flex items-center">
       <SearchIcon />
@@ -31,8 +97,11 @@ const SearchInput = () => {
         id=""
         name=""
         type="text"
-        placeholder="Search"
-        className="w-4/6 ml-2 mt-2 py-1 pl-3 pr-3 text-base text-gray-900 placeholder:text-gray-800 outline-none focus:outline-0  "
+        placeholder="City name"
+        className="w-full outline-none mr-1"
+        search={search}
+        onChange={onChangeText}
+        onKeyDown={onPressEnter}
       />
     </div>
   );
